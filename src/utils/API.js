fdf6f8efc6b9dc528da6104ff9119f20a83f8d81
Johnny Lieu <johnnylieu@yahoo.com>
2020-11-08 18:16:27 -0800

    added baseurl

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
+// const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
